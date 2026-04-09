8ac7397cbcf91ac0ffae6849082c3d3d2318f6f9
izipuho <58221294+izipuho@users.noreply.github.com>
2026-04-09 15:01:10 +0300

    table view

--- ha-device-tracker/files/www/luci-static/resources/view/ha-device-tracker/config.js
+++ ha-device-tracker/files/www/luci-static/resources/view/ha-device-tracker/config.js
@@ -45,10 +45,15 @@ return view.extend({
 		o.placeholder = '*-main-*';
 		o.depends('track_all_ifaces', '0');
 
-		s = m.section(form.TypedSection, 'device', _('Tracked devices'));
+		s = m.section(form.GridSection, 'device', _('Tracked devices'));
 		s.anonymous = false;
 		s.addremove = true;
 		s.sortable = true;
+		s.nodescriptions = true;
+		s.modaltitle = _('Tracked device');
+		s.sectiontitle = function(section_id) {
+			return _('Device') + ' ' + section_id;
+		};
 
 		o = s.option(form.Value, 'mac', _('MAC address'));
 		o.datatype = 'macaddr';
